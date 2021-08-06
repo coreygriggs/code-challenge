90c681a5354235955a19891345eb186406453e02
Corey Griggs <griggscorey@gmail.com>
2021-08-05 22:28:49 -0500

    api url

--- code-challenge/src/services/ApiService.ts
+++ code-challenge/src/services/ApiService.ts
@@ -1,8 +1,7 @@
 export default class ApiService {
     // we are using proxy locally, I'd make urls an env var
     BASE_NYT_URL = 'https://api.nytimes.com';
-    API_URL: string = `${window.location.host === 'localhost:3000' ? 
-        '' : this.BASE_NYT_URL}/svc/topstories/v2/science.json?api-key=Gwxln5M3geWlhR6UE0TY1FUWKSG3wCil`;
+    API_URL: string = `${this.BASE_NYT_URL}/svc/topstories/v2/science.json?api-key=Gwxln5M3geWlhR6UE0TY1FUWKSG3wCil`;
 
     async get() {
         return await fetch(this.API_URL)
